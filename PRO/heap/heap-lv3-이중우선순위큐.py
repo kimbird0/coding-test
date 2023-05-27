e05a55efb09b347ae141aebf5409cf0cc629b59f
birdzero <redviking2@naver.com>
2023-05-27 23:42:24 +0900

    [PRO] heap lv3 이중우선순위큐

---
+++ PRO/heap/heap-lv3-이중우선순위큐.py
@@ -0,0 +1,23 @@
+#프로그래머스 고득점 kit heap
+#이중우선순위큐
+
+import heapq
+
+def solution(operations):
+    h = []
+
+    for i in operations:
+        a, b = i.split()
+        if a == 'I':
+            heapq.heappush(h, int(b))
+        else:
+            if len(h) > 0:
+                if b == '1':
+                    h.pop(h.index(heapq.nlargest(1, h)[0]))
+                else:
+                    heapq.heappop(h)
+
+    if len(h) == 0:
+        return [0, 0]
+    else:
+        return [heapq.nlargest(1, h)[0], h[0]]
