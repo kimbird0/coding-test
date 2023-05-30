f0462c4ec8cc71d89a106d6a2afd8aa16995217f
birdzero <redviking2@naver.com>
2023-05-30 23:36:09 +0900

    [PRO] greedy algo lv3 단속 카메라

---
+++ PRO/greedy/greedy-lv3-단속카메라.py
@@ -0,0 +1,13 @@
+#프로그래머스 고득점 kit 그리디 알고리즘
+#단속 카메라
+
+def solution(routes):
+    answer = 0
+    routes.sort(key=lambda x: x[1]) # routes를 차량이 나간 지점 (진출) 기준으로 정렬
+    camera = -30001 # -30001부터 카메라 위치를 찾습니다.
+
+    for route in routes:
+        if camera < route[0]:
+            answer += 1
+            camera = route[1]
+    return answer
